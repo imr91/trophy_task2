bd77fad2a9c5e5ba2fd592d9cc4939ab6c80ddc8
Imre Horvath <123546876+imr91@users.noreply.github.com>
2023-07-01 20:28:56 +0200

    Add description and score board
    - create left panel with description and score board
    - create mysql handling for the score board
    - add text to description

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    $con = new mysqli("127.0.0.1", "game_admin", "game202306", "trophy_task2");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,9 +20,43 @@
 
 </head>
 <body>
+    <div class='container'>
+    <div class='score' >
+        <div id='description'>
+            <p>Tomato Run</p>
+            Tomato would like to meet its family. Navigate with arrow keys to help Tomato and use spacebar to go even faster!
+        </div>    
+    
+        <table id='scoretable'>
+            <caption>Score Board</caption>
+            <thead>
+                <tr>
+                    <td>No.</td>
+                    <td>Name</td>
+                    <td>Score</td>
+                    <td>Date</td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                   $query = "SELECT * FROM game_scores ORDER BY score DESC LIMIT 10";
+                    $result = $con -> query($query);
+                    $num =1;
+                    if ($result -> num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<tr><td>".$num."</td><td>".$row['player_name']."</td><td class='scorenum'>".$row['score']."</td><td>".$row['score_date']."</td></tr>";
+                            $num++;
+                        };
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
     <div style='position:relative;width:100%;height:100%;'>
         <div id='map' style='width:100%;height:100vh;will-change: transform;'>
     </div>
+    </div>
+
 </div>
 </body>
 </html>
