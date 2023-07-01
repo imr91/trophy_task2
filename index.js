829ad8c4b915e8477ca0a344c8ca116baddbec99
Imre Horvath <123546876+imr91@users.noreply.github.com>
2023-07-01 15:28:00 +0200

    Starting tomato and finish area
    - create finish area with 1 km radius red circle
    - create tomato marker with tomato icon at starting point

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var finishCircle = L.circle([55.232816, 11.767130], {
+    color: 'red',
+    fillColor: '#f04',
+    fillOpacity: 0.35,
+    radius: 1000
+}).addTo(map);
+
+var tomatoIcon = L.icon({
+    iconUrl: 'tomato-svgrepo-com.svg',
+    iconSize: [30,30],
+    iconAnchor: [15,15]
+})
+
+L.marker([lat, lon], {icon: tomatoIcon}).addTo(map);
