c5427b9fc86b3841b99a17cf61ea052791364356
Imre Horvath <123546876+imr91@users.noreply.github.com>
2023-07-05 16:56:18 +0200

    SQL database for the game

--- trophy_task2.sql
+++ trophy_task2.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `trophy_task2` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `trophy_task2`;
 -- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
 --
 -- Host: localhost    Database: trophy_task2
@@ -80,4 +82,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-05 16:52:03
+-- Dump completed on 2023-07-05 16:55:33
